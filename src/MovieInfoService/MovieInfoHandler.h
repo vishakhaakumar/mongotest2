91b7a1472041c82b27439b69ba87f6f52a41e2fa
vishakhaakumar <79542888+vishakhaakumar@users.noreply.github.com>
2021-03-30 13:50:06 -0400

    Update MovieInfoHandler.h

--- src/MovieInfoService/MovieInfoHandler.h
+++ src/MovieInfoService/MovieInfoHandler.h
@@ -16,23 +16,41 @@ namespace movies{
 
 class MovieInfoServiceHandler : public MovieInfoServiceIf {
  public:
-  MovieInfoServiceHandler();
+  MovieInfoServiceHandler(ClientPool<ThriftClient<MovieInfoServiceClient>> *);
   ~MovieInfoServiceHandler() override=default;
 
   void GetMoviesByIds(std::vector<std::string>& _return, const std::vector<std::string> & movie_ids) override;
+  private:
+  ClientPool<ThriftClient<MovieInfoServiceClient>> *_movie_mongodb_client_pool;
 };
 
 // Constructor
-MovieInfoServiceHandler::MovieInfoServiceHandler() {
-
+MovieInfoServiceHandler::MovieInfoServiceHandler(
+ClientPool<ThriftClient<MovieInfoServiceClient>> *movie_mongodb_client_pool) {
+  _movie_mongodb_client_pool = movie_mongodb_client_pool;
 }
 
 // Remote Procedure "GetMoviesById"
 
 void MovieInfoServiceHandler::GetMoviesByIds(std::vector<std::string>& _return, const std::vector<std::string> & movie_ids) {
    printf("GetMoviesByIds called \n");
+  std::cout << "GetMoviesByIds here ...!!!!!!!!" << std::endl;
+ 
+ // testing  Get the movie info service client pool
+    auto movie_info_client_wrapper = movie_mongodb_client_pool->Pop();
+    if (!movie_info_client_wrapper) {
+	   std::cout << "ERROR here ..." << std::endl;
+      ServiceException se;
+      se.errorCode = ErrorCode::SE_THRIFT_CONN_ERROR;
+      se.message = "Failed to connect to movie-info-service";
+      throw se;
+    }
+	std::cout << "Client pool pop done !!! ..." << std::endl;
+    auto movie_info_client = movie_info_client_wrapper->GetClient();
+	std::cout << "GetClient done !!! ..." << std::endl;
+ 
    _return.push_back("Muppets Take Manhattan I");
-   _return.push_back("The Lion King II");
+   _return.push_back("The Lion Kingi II");
 }
 
 } // namespace movies
