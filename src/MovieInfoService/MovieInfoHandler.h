35f4902d8c8be4a78300a662a14a619246b0ad54
vishakhaakumar <79542888+vishakhaakumar@users.noreply.github.com>
2021-03-31 11:06:40 -0400

    Update MovieInfoHandler.h

--- src/MovieInfoService/MovieInfoHandler.h
+++ src/MovieInfoService/MovieInfoHandler.h
@@ -8,14 +8,10 @@
 
 #include <cstdint>
 #include <vector>
-#include <bsoncxx/json.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/stdx.hpp>
 #include <mongocxx/uri.hpp>
 #include <mongocxx/instance.hpp>
-#include <bsoncxx/builder/stream/helpers.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/builder/stream/array.hpp>
 
 #include "../../gen-cpp/MovieInfoService.h"
 
